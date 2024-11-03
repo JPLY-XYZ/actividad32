5364a509ad7f49afe063dcc122f5b89167d742da
JPLY-XYZ <josecabeza.compras@gmail.com>
2024-11-03 10:49:07 +0100

    inicializacion proyecto

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+
+
+const nextConfig = {
+    images: {
+      domains: ['www.juntadeandalucia.es'], 
+    },
+    images: {
+        domains: ['www.pixabay.com'], 
+      },
+   
+  };
+
+  
+export default nextConfig;
