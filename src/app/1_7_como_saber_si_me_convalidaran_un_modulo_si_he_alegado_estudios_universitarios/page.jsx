910e74d6231cd712972143d0b6b25ff878f7e7b3
JPLY-XYZ <josecabeza.compras@gmail.com>
2024-11-15 11:13:37 +0100

    añadiendo nuevas pags

--- src/app/1_7_como_saber_si_me_convalidaran_un_modulo_si_he_alegado_estudios_universitarios/page.jsx
+++ src/app/1_7_como_saber_si_me_convalidaran_un_modulo_si_he_alegado_estudios_universitarios/page.jsx
@@ -1,19 +1,36 @@
 import Titulo from "@/components/titulo";
 import Nav from "@/components/nav";
 
-
 export const metadata = {
-  title: "1.7.- ¿Cómo saber si me convalidarán un módulo si he alegado estudios universitarios?"
+  title:
+    "1.7.- ¿Cómo saber si me convalidarán un módulo si he alegado estudios universitarios?",
 };
 
 export default function Home() {
   return (
     <>
-      <Titulo>  { metadata.title } </Titulo>
+      <Titulo> {metadata.title} </Titulo>
       <Nav pos={17} />
 
       <div className="flex flex-col gap-4">
-
+        <p>No se puede saber a priori.</p>
+        <p>
+          Por eso el Ministerio de Educación y Formación Profesional solicita
+          los programas universitarios sellados: para poder analizar caso a caso
+          y de forma individualizada cada convalidación.
+        </p>
+        <p>
+          El Ministerio de Educación y Formación Profesional tiene en su página
+          Web información sobre la convalidación de módulos profesionales
+          aportando estudios universitarios. La podéis encontrar en este
+          <a
+            className="font-bold text-blue-500 underline"
+            href="https://www.todofp.es/convalidaciones-equivalencias-homologaciones/convalidaciones.html"
+          >
+            enlace
+          </a>
+          .
+        </p>
       </div>
     </>
   );
